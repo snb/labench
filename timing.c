2ba530a278e0cf6ac32628b23604397148efa89a
Nick Barkas <snb@freebsd.org>
2009-09-08 14:36:56 +0200

    Now with delicious BeerWare boilerplate!
    Thanks to Poul-Henning Kamp for the awesome license.

--- timing.c
+++ timing.c
@@ -1,3 +1,12 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <snb@FreeBSD.org> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. -Nick Barkas
+ * ----------------------------------------------------------------------------
+ */
+
 #include <stdio.h>
 #include <time.h>
 
